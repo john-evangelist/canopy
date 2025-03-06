6d3e35705a499bac20e09285c4d270d0f82a601e
Evangelista <evangelistajoaop@gmail.com>
2025-03-06 14:36:07 -0300

    Generate control plane

---
+++ control_plane/mix.exs
@@ -0,0 +1,32 @@
+defmodule ControlPlane.MixWorkspace do
+    use Mix.Project
+
+    def project do
+      [
+        app: :control_plane,
+        version: "0.1.0",
+        elixir: "~> 1.18",
+        start_permanent: Mix.env() == :prod,
+        deps: deps(),
+        elixirc_paths: [],
+        workspace: [
+          type: :workspace
+        ],
+        lockfile: "workspace.lock"
+      ]
+    end
+
+    # Run "mix help compile.app" to learn about applications.
+    def application do
+      [
+        extra_applications: []
+      ]
+    end
+
+    # Run "mix help deps" to learn about dependencies.
+    defp deps do
+      [
+        {:workspace, "~> 0.2.0"}
+      ]
+    end
+  end
